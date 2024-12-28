9d69d9cd485d97750e85d15dd9c40c11491cf248
Litesh121 <158590657+Litesh121@users.noreply.github.com>
2024-12-28 17:59:26 +0530

    Update database.js

--- paytm-clone/backend/src/config/database.js
+++ paytm-clone/backend/src/config/database.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const { login } = require('../controllers/userController');
+login
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/paytm_clone', {
+        await mongoose.connect('mongodb://127.0.0.1:27017', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
@@ -12,5 +19,19 @@ const connectDB = async () => {
         process.exit(1);
     }
 };
+
+
+
+const tuteschema=new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+
+const collection=new mongoose.model('data',tuteschema);
+
+collection.insertMany([])
 
 module.exports = connectDB;
